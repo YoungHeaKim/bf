39389bfa1976bc5c02022ac90d0ec806b6b2b797
YoungHeaKim <dydgo9507@gmail.com>
2021-02-27 15:21:54 +0900

    거래처 디테일 페이지 분기 이동 추가 및 table list 변경

--- src/Main/views/Store/index.jsx
+++ src/Main/views/Store/index.jsx
@@ -10,8 +10,8 @@ const cx = classNames.bind(styles);
 const Store = () => {
   const [store, setStore] = useState('');
   const [orders, setOrders] = useState([]);
-  const year = moment().format('YYYY');
-  const month = moment().format('MM');
+  const [year, setYear] = useState('');
+  const [quarter, setQuarter] = useState('');
 
   useEffect(() => {
     // 가게 정보 api로 가져오기(store)
@@ -21,6 +21,7 @@ const Store = () => {
       name: '동천',
       owner: '주인이름',
       bizNum: '123-45-6788',
+      phoneNumber: '010-1234-5678',
       address: '서울시 서초구 방배로11길 35',
     });
 
@@ -51,7 +52,36 @@ const Store = () => {
         totalPrice: 40000,
       },
     ]);
+
+    setYear(moment().format('YYYY'));
+
+    const month = moment().format('MM');
+
+    if (0 < Number(month) && Number(month) <= 3) setQuarter('1분기');
+    else if (3 < Number(month) && Number(month) <= 6) setQuarter('2분기');
+    else if (6 < Number(month) && Number(month) <= 9) setQuarter('3분기');
+    else if (9 < Number(month)) setQuarter('4분기');
   }, []);
+
+  const nextQuarter = () => {
+    if (quarter === '1분기') setQuarter('2분기');
+    if (quarter === '2분기') setQuarter('3분기');
+    if (quarter === '3분기') setQuarter('4분기');
+    if (quarter === '4분기') {
+      setYear(Number(year) + 1);
+      setQuarter('1분기');
+    }
+  };
+
+  const prevQuarter = () => {
+    if (quarter === '2분기') setQuarter('1분기');
+    if (quarter === '3분기') setQuarter('2분기');
+    if (quarter === '4분기') setQuarter('3분기');
+    if (quarter === '1분기') {
+      setYear(Number(year) - 1);
+      setQuarter('4분기');
+    }
+  };
 
   return (
     <Fragment>
@@ -59,21 +89,33 @@ const Store = () => {
         <div className={cx('store__tr', 'store__header')}>
           <div className={cx('store__td')}>닉네임</div>
           <div className={cx('store__td')}>상호</div>
-          <div className={cx('store__td')}>사업자 번호</div>
-          <div className={cx('store__td')}>대표자</div>
+          <div className={cx('store__td')}>전화 번호</div>
           <div className={cx('store__td')}>주소</div>
         </div>
         <List className={cx('store__tr')}>
           <div className={cx('store__td')}>{store.nickName}</div>
           <div className={cx('store__td')}>{store.name}</div>
-          <div className={cx('store__td')}>{store.bizNum}</div>
-          <div className={cx('store__td')}>{store.owner}</div>
+          <div className={cx('store__td')}>{store.phoneNumber}</div>
           <div className={cx('store__td')}>{store.address}</div>
         </List>
       </div>
       <div className={cx('store__detail__title')}>
+        <button
+          className={cx('store__prev__btn')}
+          onClick={prevQuarter}
+          type="button"
+        >
+          &lt;
+        </button>
+        <button
+          className={cx('store__next__btn')}
+          onClick={nextQuarter}
+          type="button"
+        >
+          &gt;
+        </button>
         <div className={cx('detail__title')}>
-          {year}년 {/* TODO: 이부분 분기 나누는 부분 수정해야함 */}
+          {year}년 {quarter}
         </div>
         <div className={cx('detail__title')}>{store.nickName} 분기별 장부</div>
       </div>
